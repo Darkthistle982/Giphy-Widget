5e725a7133ec2ded9a2dcb6d0162991e6f67cff1
Brian Allen <brianallen2401@gmail.com>
2020-01-31 14:07:04 -0700

    moved the queryURL and ajax into the button click function. tested with a log. began writing logic to loop out and create divs for the gif and rating to inject into the DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,27 +2,35 @@
 var topics = ["critical role", "dungeons and dragons", "star wars", "animals being jerks", "star trek", "cats", "alice cooper", "the muppets", "nat 20", "lord of the rings"]
 //function to create buttons from the arary
 function createButton() {
-    for(i = 0; i < topics.length; i++) {
+    for (i = 0; i < topics.length; i++) {
         var newButton = $("<button>").text(topics[i]);
         $("#button-goes-here").prepend(newButton);
-    }    
+    }
 }
 createButton();
-// var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FIUPsuC3wpsjInI9z00DHj7Zw7Ye60Q2&q=" + topics[i] + "&limit=10&offset=0&rating=PG-13&lang=en";
+//on click function linked to button presses
+$("button").on("click", function () {
+    //variable for the query URL to the GIPHY api with the index of the topic chosen
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FIUPsuC3wpsjInI9z00DHj7Zw7Ye60Q2&q=" + topics[i] + "&limit=10&offset=0&rating=PG-13&lang=en";
+    //ajax call to the API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        //function to get the data from the API and inject it into the DOM
+        var results = response.data;
+        for (i = 0; i < results.length; i++) {
+            var buttonDiv = $("<div>");
+            var rating = results[i].rating;
+            var ratingText = $("<p>").text("Rating: " + rating);
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function(response) {
+        }
+        
 
 
 
 
 
+    });
 
-
-
-
-
-
-// });
+});
