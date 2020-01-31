ffd376ec9473752fac5a65446259375e45ddef0f
Brian Allen <brianallen2401@gmail.com>
2020-01-31 13:08:32 -0700

    added basic array to hold topics. Created the query URL variable with the link to the GIPHY API. built basic ajac call.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,26 @@
+//variable array to hold the topics on which to query the giphy api and return results. Also holds input for new button created by users
 var topics = ["critical role", "dungeons and dragons", "star wars", "animals being jerks", "star trek", "cats", "alice cooper", "the muppets", "nat 20", "lord of the rings"]
+//function to create buttons from the arary
+function createButton() {
+    var newButton = $("<button>");
+    
+}
 
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FIUPsuC3wpsjInI9z00DHj7Zw7Ye60Q2&q=" + topics[i] + "&limit=10&offset=0&rating=PG-13&lang=en";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+
+
+
+
+
+
+
+
+
+
+
+});
